8ea2de0a78d08884fff7ca61811deb7d5754d681
bigtendons8 <154099045+bigtendons8@users.noreply.github.com>
2024-07-28 12:23:08 +1000

    formatting

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import simplegrid
 import time
 
-grid = simplegrid.Grid(75, 75, "■ ")
-direction = 90
-grid.selected_x = 40
-grid.selected_y = 40
+def main():
 
-while True:
-    if grid.read_here() == "■ ":
-        grid.change_here("□ ")
-        direction += 90
-    else:
-        grid.change_here("■ ")
-        direction -= 90
+    grid = simplegrid.Grid(75, 75, "■ ")
+    direction = 90
+    grid.selected_x = 40
+    grid.selected_y = 40
 
-    normalised = direction % 360
-    if normalised == 90:
-        grid.move(1, 0)
-    elif normalised == 180:
-        grid.move(0, 1)
-    elif normalised == 270:
-        grid.move(-1, 0)
-    elif normalised == 0:
-        grid.move(0, -1)
+    while True:
+        if grid.read_here() == "■ ":
+            grid.change_here("□ ")
+            direction += 90
+        else:
+            grid.change_here("■ ")
+            direction -= 90
 
-    print(grid, flush=True)
-    time.sleep(0.01)
+        normalised = direction % 360
+        if normalised == 90:
+            grid.move(1, 0)
+        elif normalised == 180:
+            grid.move(0, 1)
+        elif normalised == 270:
+            grid.move(-1, 0)
+        elif normalised == 0:
+            grid.move(0, -1)
+
+        print(grid, flush=True)
+        time.sleep(0.01)
+
+
+if __name__ == "__main__":
+    main()
+
